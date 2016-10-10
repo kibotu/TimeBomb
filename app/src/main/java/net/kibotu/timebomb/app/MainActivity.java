896b7ca21b237cd7fbe1494a459af6969fc90c44
Jan Rabe <jan.rabe@exozet.com>
2016-10-10 19:27:44 +0200

    added convenience method, also displaying left days in demo app

--- app/src/main/java/net/kibotu/timebomb/app/MainActivity.java
+++ app/src/main/java/net/kibotu/timebomb/app/MainActivity.java
@@ -2,8 +2,8 @@ package net.kibotu.timebomb.app;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
 
-import net.kibotu.timebomb.BuildConfig;
 import net.kibotu.timebomb.TimeBomb;
 
 public class MainActivity extends AppCompatActivity {
@@ -13,6 +13,9 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        TimeBomb.bombAfterDays(this, Long.parseLong(BuildConfig.BUILD_DATE), 14);
+        TimeBomb.enableLogging(BuildConfig.DEBUG);
+        long timeLeft = TimeBomb.bombAfterDays(this, BuildConfig.BUILD_DATE, 14);
+
+        ((TextView) findViewById(R.id.label)).setText(timeLeft + " days left until bombing.");
     }
 }
--- lib/src/main/java/net/kibotu/timebomb/TimeBomb.java
+++ lib/src/main/java/net/kibotu/timebomb/TimeBomb.java
@@ -8,6 +8,7 @@ import android.util.Log;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.concurrent.TimeUnit;
 
 /**
  * Created by <a href="https://about.me/janrabe">Jan Rabe</a>.
@@ -17,24 +18,58 @@ public class TimeBomb {
 
     private static final String TAG = TimeBomb.class.getSimpleName();
 
+    private static boolean enableLogging = false;
+
+    private TimeBomb() throws IllegalAccessException {
+        throw new IllegalAccessException();
+    }
+
+    /**
+     * Blocks User from keep using the app.
+     *
+     * @param context   Context
+     * @param buildTime Unix Timestamp.
+     * @param unit      Amount of days after build time when the blocking should be triggered.
+     * @param timeUnit  {@link TimeUnit}
+     * @return Returns left time until the app will be blocked.
+     */
+    public static long bombAfterDays(Context context, String buildTime, long unit, TimeUnit timeUnit) {
+        return bombAfterDays(context, Long.parseLong(buildTime), (int) timeUnit.toDays(unit));
+    }
+
     /**
      * Blocks User from keep using the app.
      *
      * @param context   Context
      * @param buildTime Unix Timestamp.
      * @param days      Amount of days after build time when the blocking should be triggered.
+     * @return Returns left time until the app will be blocked.
      */
-    public static void bombAfterDays(Context context, long buildTime, int days) {
+    public static long bombAfterDays(Context context, String buildTime, int days) {
+        return bombAfterDays(context, Long.parseLong(buildTime), days);
+    }
+
+    /**
+     * Blocks User from keep using the app.
+     *
+     * @param context   Context
+     * @param buildTime Unix Timestamp.
+     * @param days      Amount of days after build time when the blocking should be triggered.
+     * @return Returns left time until the app will be blocked.
+     */
+    public static long bombAfterDays(Context context, long buildTime, int days) {
         SimpleDateFormat sdf1 = new SimpleDateFormat("dd.MM.yyyy");
         Calendar calendar = Calendar.getInstance();
         calendar.setTimeInMillis(buildTime);
         Date buildDate = calendar.getTime();
         Date today = new Date();
         long difference = daysBetween(buildDate, today);
-        Log.v(TAG, "[bombAfterDays] Build Date=" + sdf1.format(buildDate) + " Today=" + sdf1.format(today) + " Passed Days=" + difference);
+
+        long timeLeft = days - difference;
+        Log.v(TAG, "[bombAfterDays] Bomb in " + timeLeft + "d Build Date=" + sdf1.format(buildDate) + " Today=" + sdf1.format(today) + " Passed Days=" + difference);
 
         if (difference <= days)
-            return;
+            return timeLeft;
 
         AlertDialog.Builder builder = new AlertDialog.Builder(context);
         builder.setMessage(R.string.time_bomb_message)
@@ -46,6 +81,8 @@ public class TimeBomb {
             ((AlertDialog) dialog1).getButton(AlertDialog.BUTTON_NEGATIVE).setEnabled(false);
         });
         dialog.show();
+
+        return timeLeft;
     }
 
     private static Calendar getDatePart(Date date) {
@@ -71,5 +108,13 @@ public class TimeBomb {
             daysBetween++;
         }
         return daysBetween;
+    }
+
+    public static boolean enableLogging() {
+        return enableLogging;
+    }
+
+    public static void enableLogging(boolean enableLogging) {
+        TimeBomb.enableLogging = enableLogging;
     }
 }
